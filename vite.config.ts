2525eb57ec4d5ed400b197409e517812f3274db0
Othmane Menad <menad.othmane@gmail.com>
2023-07-27 16:28:33 +0100

    Implemented the paths module for easy improrting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // everytime you write @ it will replace it with the __direname ( current directory folder) and we gonnaadd src to it
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});

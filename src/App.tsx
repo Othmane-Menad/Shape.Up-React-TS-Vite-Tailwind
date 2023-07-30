47578fcb090df875218e35307507b968d9dafa2e
Othmane Menad <menad.othmane@gmail.com>
2023-07-30 17:34:16 +0100

    NavBar Completed fixed  transparent with color change on scroll and mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
-function App() {
+import { SelectedPage } from "./shared/types";
 
+//moved to shared folder
+/* enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+} */
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY == 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-<div className="app bg-gray-20">
-  <Navbar />
-</div>
-  )
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,18 +1,20 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
-  selectedPage: String;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
   // just turning the page name to lowercase and removing spaces, to be used as an ID
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  // as keyword to represent the variable as SelectedPage enum
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
       className={`${
-        selectedPage === lowerCasePage ? text - primary - 500 : ""
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
       } transition duration-500 hover:text-primary-300`}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
